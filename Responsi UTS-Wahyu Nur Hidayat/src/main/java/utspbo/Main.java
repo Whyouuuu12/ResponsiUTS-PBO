f06d678a73cb95da835e5b9bd165793908e629ca
Wahyu Nur Hidayat <wh664748@gmail.com>
2024-10-01 10:38:01 +0700

    Upload ResponsiUTS PBO

---
+++ Responsi UTS-Wahyu Nur Hidayat/src/main/java/utspbo/Main.java
@@ -0,0 +1,192 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package utspbo;
+
+/**
+ *
+ * @author hp
+ */
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author hp
+ */
+class Produk {
+    private String namaProduk;
+    private double harga;
+
+    public Produk(String namaProduk, double harga) {
+        this.namaProduk = namaProduk;
+        this.harga = harga;
+    }
+
+    public String getNamaProduk() {
+        return namaProduk;
+    }
+
+    public void setNamaProduk(String namaProduk) {
+        this.namaProduk = namaProduk;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+
+    public void tampilkanInfo() {
+        System.out.println("Nama Produk: " + namaProduk);
+        System.out.println("Harga: " + String.format("%.0f", harga));
+    }
+}
+
+
+class Elektronik extends Produk {
+    private int garansi; // dalam tahun
+
+    public Elektronik(String namaProduk, double harga, int garansi) {
+        super(namaProduk, harga);
+        this.garansi = garansi;
+    }
+
+    public int getGaransi() {
+        return garansi;
+    }
+
+    public void setGaransi(int garansi) {
+        this.garansi = garansi;
+    }
+
+    @Override
+    public void tampilkanInfo() {
+        super.tampilkanInfo();
+        System.out.println("Garansi: " + garansi + " tahun");
+    }
+}
+
+class Makanan extends Produk {
+    private String tanggalKadaluarsa;
+
+    public Makanan(String namaProduk, double harga, String tanggalKadaluarsa) {
+        super(namaProduk, harga);
+        this.tanggalKadaluarsa = tanggalKadaluarsa;
+    }
+
+    public String getTanggalKadaluarsa() {
+        return tanggalKadaluarsa;
+    }
+
+    public void setTanggalKadaluarsa(String tanggalKadaluarsa) {
+        this.tanggalKadaluarsa = tanggalKadaluarsa;
+    }
+
+    @Override
+    public void tampilkanInfo() {
+        super.tampilkanInfo();
+        System.out.println("Tanggal Kadaluarsa: " + tanggalKadaluarsa);
+    }
+}
+
+class Pegawai {
+    private String namaPegawai;
+    private double gaji;
+
+    public Pegawai(String namaPegawai, double gaji) {
+        this.namaPegawai = namaPegawai;
+        this.gaji = gaji;
+    }
+
+    public String getNamaPegawai() {
+        return namaPegawai;
+    }
+
+    public void setNamaPegawai(String namaPegawai) {
+        this.namaPegawai = namaPegawai;
+    }
+
+    public double getGaji() {
+        return gaji;
+    }
+
+    public void setGaji(double gaji) {
+        this.gaji = gaji;
+    }
+
+    public void tampilkanInfo() {
+        System.out.println("Nama Pegawai: " + namaPegawai);
+        System.out.println("Gaji: " + gaji);
+    }
+}
+
+class PegawaiTetap extends Pegawai {
+    private double tunjangan;
+
+    public PegawaiTetap(String namaPegawai, double gaji, double tunjangan) {
+        super(namaPegawai, gaji);
+        this.tunjangan = tunjangan;
+    }
+
+    public double getTunjangan() {
+        return tunjangan;
+    }
+
+    public void setTunjangan(double tunjangan) {
+        this.tunjangan = tunjangan;
+    }
+
+    @Override
+    public void tampilkanInfo() {
+        super.tampilkanInfo();
+        System.out.println("Tunjangan: " + tunjangan);
+    }
+}
+
+class PegawaiKontrak extends Pegawai {
+    private int lamaKontrak; // dalam bulan
+
+    public PegawaiKontrak(String namaPegawai, double gaji, int lamaKontrak) {
+        super(namaPegawai, gaji);
+        this.lamaKontrak = lamaKontrak;
+    }
+
+    public int getLamaKontrak() {
+        return lamaKontrak;
+    }
+
+    public void setLamaKontrak(int lamaKontrak) {
+        this.lamaKontrak = lamaKontrak;
+    }
+
+    @Override
+    public void tampilkanInfo() {
+        super.tampilkanInfo();
+        System.out.println("Lama Kontrak: " + lamaKontrak + " bulan");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Polimorfisme untuk Produk
+        Produk produk1 = new Elektronik("Laptop", 15000000, 2);
+        Produk produk2 = new Makanan("Snack", 15000, "30-12-2023");
+
+        // Polimorfisme untuk Pegawai
+        Pegawai pegawai1 = new PegawaiTetap("Wahyu", 5000000, 1000000);
+        Pegawai pegawai2 = new PegawaiKontrak("Dayat", 3000000, 12);
+
+        // Memanggil metode tampilkanInfo() dengan referensi induk
+        produk1.tampilkanInfo();
+        produk2.tampilkanInfo();
+        pegawai1.tampilkanInfo();
+        pegawai2.tampilkanInfo();
+    }
+}
+
+
